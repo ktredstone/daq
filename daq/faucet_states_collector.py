4da8cd3e483ff479138e46766c48380e99f38dba
Yufeng Duan <55268016+didovesei@users.noreply.github.com>
2019-09-24 12:43:18 -0700

    Fixes: lint, ISO timestamp, map entry variable name (#203)

--- daq/faucet_states_collector.py
+++ daq/faucet_states_collector.py
@@ -1,10 +1,11 @@
-from collections import namedtuple
+"""Processing faucet events"""
 from datetime import datetime
 
 
-def save_states(f):
+def save_states(func):
+    """Decorator to dump the current states after the states map is modified"""
     def wrapped(self, *args, **kwargs):
-        res = f(self, *args, **kwargs)
+        res = func(self, *args, **kwargs)
         print(self.system_states)
         return res
 
@@ -12,55 +13,65 @@ def save_states(f):
 
 
 class FaucetStatesCollector:
+    """Processing faucet events and store states in the map"""
 
-    TABLE_ENTRY_SWITCH = "dpid"
-    TABLE_ENTRY_PORTS = "ports"
-    TABLE_ENTRY_PORT_STATE_CHANGED_COUNT = "change_count"
-    TABLE_ENTRY_PORT_STATUS = "status"
-    TABLE_ENTRY_PORT_LAST_CHANGED_TS = "last_changed_timestamp"
-    TABLE_ENTRY_PORT_LEARNED_MACS = "learned_macs"
-    TABLE_ENTRY_PORT_MAC_LEARNING_TS = "timestamp"
-    TABLE_ENTRY_CONFIG_CHANGE_COUNT = "change_count"
-    TABLE_ENTRY_LAST_RESTART_TYPE = "last_restart"
-    TABLE_ENTRY_LAST_RESTART_TS = "last_restart_timestamp"
+    MAP_ENTRY_SWITCH = "dpids"
+    MAP_ENTRY_PORTS = "ports"
+    MAP_ENTRY_PORT_STATE_CHANGED_COUNT = "change_count"
+    MAP_ENTRY_PORT_STATUS = "status"
+    MAP_ENTRY_PORT_LAST_CHANGED_TS = "last_changed_timestamp"
+    MAP_ENTRY_PORT_LEARNED_MACS = "learned_macs"
+    MAP_ENTRY_PORT_MAC_LEARNING_TS = "timestamp"
+    MAP_ENTRY_CONFIG_CHANGE_COUNT = "change_count"
+    MAP_ENTRY_LAST_RESTART_TYPE = "last_restart"
+    MAP_ENTRY_LAST_RESTART_TS = "last_restart_timestamp"
 
     def __init__(self):
-        self.system_states = {FaucetStatesCollector.TABLE_ENTRY_SWITCH: {}}
-        self.switch_states = self.system_states[FaucetStatesCollector.TABLE_ENTRY_SWITCH]
+        self.system_states = {FaucetStatesCollector.MAP_ENTRY_SWITCH: {}}
+        self.switch_states = self.system_states[FaucetStatesCollector.MAP_ENTRY_SWITCH]
 
     def get_switches(self):
+        """get the system states"""
         return self.system_states
 
     @save_states
     def process_port_state(self, dpid, port, status, timestamp):
+        """process port state event"""
         port_table = self.switch_states\
             .setdefault(dpid, {})\
-            .setdefault(FaucetStatesCollector.TABLE_ENTRY_PORTS, {})\
+            .setdefault(FaucetStatesCollector.MAP_ENTRY_PORTS, {})\
             .setdefault(port, {})
 
-        port_table[FaucetStatesCollector.TABLE_ENTRY_PORT_STATUS] = status
-        port_table[FaucetStatesCollector.TABLE_ENTRY_PORT_LAST_CHANGED_TS] = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M%S")
+        port_table[FaucetStatesCollector.MAP_ENTRY_PORT_STATUS] = status
+        port_table[FaucetStatesCollector.MAP_ENTRY_PORT_LAST_CHANGED_TS] = \
+            datetime.fromtimestamp(timestamp).isoformat()
 
-        port_table[FaucetStatesCollector.TABLE_ENTRY_PORT_STATE_CHANGED_COUNT] = \
-            port_table.setdefault(FaucetStatesCollector.TABLE_ENTRY_PORT_STATE_CHANGED_COUNT, 0) + 1
+        port_table[FaucetStatesCollector.MAP_ENTRY_PORT_STATE_CHANGED_COUNT] = \
+            port_table.setdefault(
+                FaucetStatesCollector.MAP_ENTRY_PORT_STATE_CHANGED_COUNT, 0) + 1
 
     @save_states
     def process_port_learn(self, dpid, port, mac, timestamp):
+        """process port learn event"""
         mac_table = self.switch_states\
             .setdefault(dpid, {})\
-            .setdefault(FaucetStatesCollector.TABLE_ENTRY_PORTS, {})\
+            .setdefault(FaucetStatesCollector.MAP_ENTRY_PORTS, {})\
             .setdefault(port, {})\
-            .setdefault(FaucetStatesCollector.TABLE_ENTRY_PORT_LEARNED_MACS, {})\
+            .setdefault(FaucetStatesCollector.MAP_ENTRY_PORT_LEARNED_MACS, {})\
             .setdefault(mac, {})
 
-        mac_table[FaucetStatesCollector.TABLE_ENTRY_PORT_MAC_LEARNING_TS] = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M%S")
+        mac_table[FaucetStatesCollector.MAP_ENTRY_PORT_MAC_LEARNING_TS] = \
+            datetime.fromtimestamp(timestamp).isoformat()
 
     @save_states
     def process_config_change(self, dpid, restart_type, timestamp):
+        """process config change event"""
         config_change_table = self.switch_states\
             .setdefault(dpid, {})
 
-        config_change_table[FaucetStatesCollector.TABLE_ENTRY_LAST_RESTART_TYPE] = restart_type
-        config_change_table[FaucetStatesCollector.TABLE_ENTRY_LAST_RESTART_TS] = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
-        config_change_table[FaucetStatesCollector.TABLE_ENTRY_CONFIG_CHANGE_COUNT] =\
-            config_change_table.setdefault(FaucetStatesCollector.TABLE_ENTRY_CONFIG_CHANGE_COUNT, 0) + 1
+        config_change_table[FaucetStatesCollector.MAP_ENTRY_LAST_RESTART_TYPE] = restart_type
+        config_change_table[FaucetStatesCollector.MAP_ENTRY_LAST_RESTART_TS] = \
+            datetime.fromtimestamp(timestamp).isoformat()
+        config_change_table[FaucetStatesCollector.MAP_ENTRY_CONFIG_CHANGE_COUNT] = \
+            config_change_table.setdefault(
+                FaucetStatesCollector.MAP_ENTRY_CONFIG_CHANGE_COUNT, 0) + 1
