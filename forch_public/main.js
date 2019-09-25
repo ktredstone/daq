1e49cdbfd3611e931e5388c7141f4d106c8054d7
Trevor Pering <peringknife@google.com>
2019-09-25 10:13:48 -0700

    Render t1 switches in table

--- forch_public/main.js
+++ forch_public/main.js
@@ -25,24 +25,43 @@ function fetch_data(category, data_url, func) {
     .catch(rejection => console.log(rejection));
 }
 
+function find_t1_switches(switch_names) {
+  const result = [];
+  for (const switch_name of switch_names) {
+    const t1index = switch_name.indexOf('t1');
+    const switch_type = t1index >= 0 && switch_name.substring(t1index, t1index + 2);
+    if (switch_type == 't1') {
+      result.push(switch_name);
+    }
+  }
+  return result.sort();
+}
+
+function render_t1_switches(switch_left, switch_right) {
+  const temp = document.getElementById('switch_dist_row_template');
+  const table_row_html = temp.innerHTML;
+  const table_row = eval('`' + table_row_html + '`');
+  console.log('table_row', table_row)
+  const tbody_holder = document.createElement('tbody');
+  tbody_holder.innerHTML = table_row;
+  const table_element = document.querySelector('#switch_table tbody');
+  const row_element = tbody_holder.firstElementChild;
+  table_element.appendChild(row_element);
+}
+
 function populate_table() {
   console.log('Populating switches', data.switches);
-  if (!data.switches.names) {
+  switch_names = data.switches && Object.keys(data.switches)
+  if (!switch_names) {
     document.getElementById('switch_table').innerHTML = 'No switches to be found!';
     return;
   }
   
-  const switches = data.switches.names.keys();
-  console.log('switches', switches)
+  console.log('switch_names', switch_names)
 
-  const temp = document.getElementById('switch_row_template');
-  const table_row_html = temp.innerHTML;
-  const table_row = eval('`' + table_row_html + '`');
-  const row_holder = document.createElement('div');
-  row_holder.innerHTML = table_row;
-  const table_element = document.querySelector('#switch_table table');
-  const row_element = row_holder.firstElementChild;
-  table_element.appendChild(row_element);
+  t1_switches = find_t1_switches(switch_names);
+  console.log('t1_switches', t1_switches);
+  render_t1_switches(t1_switches[0], t1_switches[1]);
 }
 
 function initialize() {
