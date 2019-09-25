b85b57be393b8ec528dbab3e5b940a339097e14d
Trevor Pering <peringknife@google.com>
2019-09-24 17:40:38 -0700

    Dynamically populate switch table

--- forch_public/main.js
+++ forch_public/main.js
@@ -1,42 +1,40 @@
 
-const viewer_data = {}
+const data = {}
+let editor = null;
 
 function load_viewer() {
   const container = document.getElementById('viewer_container')
   const options = {
     mode: 'view'
   };
-  const editor = new JSONEditor(container, options);
-  return editor;
+  editor = new JSONEditor(container, options);
 }
 
-function update_editor(editor, category, json) {
+function data_update(category, json, func) {
   console.log('Updating', category, 'with', json);
-  viewer_data[category] = json;
-  editor.set(viewer_data);
+  data[category] = json;
+  editor.set(data);
+  func && func();
 }
 
-function fetch_data(editor, category, data_url) {
+function fetch_data(category, data_url, func) {
   console.log('Fetching', data_url)
   fetch(data_url)
     .then(response => response.json())
-    .then(json => update_editor(editor, category, json))
+    .then(json => data_update(category, json, func))
     .catch(rejection => console.log(rejection));
 }
 
-function initialize() {
-  console.log('initializing viewer');
-  const viewer = load_viewer();
-  fetch_data(viewer, 'overview', 'overview');
-  fetch_data(viewer, 'topology', 'topology');
-  fetch_data(viewer, 'switches', 'switches');
+function populate_table() {
+  console.log('Populating switches', data.switches);
+  if (!data.switches.names) {
+    document.getElementById('switch_table').innerHTML = 'No switches to be found!';
+    return;
+  }
+  
+  const switches = data.switches.names.keys();
+  console.log('switches', switches)
 
-  switches = {
-    'hello': {
-      'id': 10000
-    }
-  };
-  switch_name = 'hello';
   const temp = document.getElementById('switch_row_template');
   const table_row_html = temp.innerHTML;
   const table_row = eval('`' + table_row_html + '`');
@@ -45,4 +43,12 @@ function initialize() {
   const table_element = document.querySelector('#switch_table table');
   const row_element = row_holder.firstElementChild;
   table_element.appendChild(row_element);
+}
+
+function initialize() {
+  console.log('initializing viewer');
+  load_viewer();
+  fetch_data('overview', 'overview');
+  fetch_data('topology', 'topology');
+  fetch_data('switches', 'switches', populate_table);
 }
